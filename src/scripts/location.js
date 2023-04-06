4afc3d44662c3ff5fd84a7e1013cb944d34f8a70
Rafael H Gomes <rafaelhgomes15@gmail.com>
2023-04-05 21:54:10 -0300

    thinking

--- src/scripts/location.js
+++ src/scripts/location.js
@@ -1,6 +1,6 @@
 import { apiKey } from '../../apiKey.js';
 
-const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&language=pt-br`
+const locationUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&language=pt-br`
 
 const searchInput = document.getElementById('search')
 const form = document.querySelector('.search-input form')
@@ -8,12 +8,15 @@ const submitButton = document.querySelector('.submit-icon')
 const loader = document.querySelector('.loader')
 const resultsDropdown = document.querySelector('.results-dropdown')
 
-const getSearchResult = () => {
+const resultKeys = [];
+const activeCards = [];
+
+const getSearchResult = async (callback) => {
 
   submitButton.style.display = 'none'
   loader.style.display = 'flex'
 
-  fetch(`${url}&q=${searchInput.value.toLowerCase()}`)
+  fetch(`${locationUrl}&q=${searchInput.value.toLowerCase()}`)
     .then(response => {
       
       submitButton.style.display = ''
@@ -26,7 +29,7 @@ const getSearchResult = () => {
 
       searchResults.push(data)
       
-      console.log(searchResults)
+      // console.log(searchResults)
 
       resultsDropdown.innerHTML = ``
 
@@ -46,23 +49,79 @@ const getSearchResult = () => {
               <p class="result-city">${result.LocalizedName}</p>
               <p class="result-uf">${result.AdministrativeArea.ID}</p>
               <p class="result-country">${result.AdministrativeArea.CountryID}</p>
+              <span class="result-key">${result.Key}</span>
             </div>
           `
         }
   
         resultsDropdown.style.display = 'block'
+  
       }
+      const resultLine = document.querySelectorAll('.result-line')
 
+      resultLine.forEach(line => {
+        line.addEventListener('click', function getWeatherByLocationKey() {
+          const resultKey = line.querySelector('.result-key').textContent;
+
+          searchInput.value = ''
+          resultsDropdown.style.display = ''
+          resultKeys.push(resultKey);
+
+          // console.log(resultKeys);
+
+          callback()
+        });
+      });
     })
     .catch((err) => {console.error(err)})
+    
+    // console.log(resultKeys);
+    
+  }
+  
+const teste = () => {
+  const conditions = []
 
+  for(let key of resultKeys) {
+    fetch(`https://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${apiKey}&language=pt-br&details=true`)
+      .then((response) => response.json())
+      .then(data => {
+        conditions.push(data[0])
+      })
+      .catch(err => console.error(err))
+  }
+  console.log(conditions)
+  // console.log('oi')
 }
 
-const resultLine = document.querySelector('.result-line')
+teste();
 
-const getCityKey = () => {
-  console.log('')
-}
-
-searchInput.addEventListener('change', getSearchResult)
+searchInput.addEventListener('change', function() {getSearchResult(teste)})
 form.addEventListener('submit', (event) => event.preventDefault())
+  
+export { resultKeys }
+
+
+// const conditions = []
+
+// fetch(`https://dataservice.accuweather.com/currentconditions/v1/41699?apikey=${apiKey}&language=pt-br&details=true`)
+//   .then(response => response.json())
+//   .then(data => {
+//     conditions.push(data[0])
+//   })
+//   .catch(err => console.error(err))
+
+// console.log(conditions)
+// const cardData = {
+//   cityName: 'Monte Aprazível',
+//   country: 'BR',
+//   temperature: '26',
+//   weatherCondition: 'Clear',
+//   icon: '7',
+//   wind: '14',
+//   Humidity: '60',
+//   ChanceOfRain: '0'
+// }
+
+// activeCards.push(cardData);
+// console.log(activeCards);
---
+++ src/scripts/teste.js
@@ -0,0 +1,8 @@
+import { resultKeys } from "./location";
+
+function test() {
+  console.log('oi')
+
+}
+
+test()
