316fee47ac9e9a40c7ee95bb894293eb6cf154c8
Rafael H Gomes <rafaelhgomes15@gmail.com>
2023-04-08 23:24:00 -0300

    showing cards with API data

--- src/scripts/location.js
+++ src/scripts/location.js
@@ -7,6 +7,7 @@ const form = document.querySelector('.search-input form')
 const submitButton = document.querySelector('.submit-icon')
 const loader = document.querySelector('.loader')
 const resultsDropdown = document.querySelector('.results-dropdown')
+const cardsContainer = document.querySelector('.cards-container')
 
 const resultKeys = [];
 const activeCards = [];
@@ -36,7 +37,7 @@ const getSearchResult = async (callback) => {
       if(searchResults[0].length == 0) {
         resultsDropdown.innerHTML = `
           <div class="result-line">
-            Não foram encontrados resultados para esta pesquisa, por favor tente novamente
+            Não foram encontrados resultados para esta pesquisa, por favor tente novamente!
           </div>
         `
 
@@ -67,9 +68,11 @@ const getSearchResult = async (callback) => {
           resultsDropdown.style.display = ''
           resultKeys.push(resultKey);
 
+          callback(resultKey);
+          console.log(activeCards);
+          setInterval(showCards, 200);
+          
           // console.log(resultKeys);
-
-          callback()
         });
       });
     })
@@ -77,12 +80,9 @@ const getSearchResult = async (callback) => {
     
     // console.log(resultKeys);
     
-  }
+}
   
-const createCardData = () => {
-  // const conditions = []
-
-  for(let key of resultKeys) {
+const createCardData = (key) => {
       const locationByKeyUrl = `https://dataservice.accuweather.com/locations/v1/${key}?apikey=${apiKey}&language=pt-br`;
       const currentConditionsUrl = `https://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${apiKey}&language=pt-br&details=true`;
       const forecastUrl = `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=${apiKey}&language=pt-br&details=true`;
@@ -98,56 +98,70 @@ const createCardData = () => {
         .then(data => {
           const [api1Data, api2Data, api3Data] = data;
     
-          // combina os dados das duas APIs em um único objeto
+          // combina os dados das três APIs em um único objeto
           const result = {
-            location: api1Data,
-            currentConditions: api2Data,
-            forecast: api3Data,
+            locationKey: api1Data.Key,
+            cityName: api1Data.LocalizedName,
+            country: api1Data.AdministrativeArea.CountryID,
+            temperature: api2Data[0].Temperature.Metric.Value,
+            currentCondition: api2Data[0].WeatherText,
+            weatherIcon: api2Data[0].WeatherIcon,
+            windSpeed: api2Data[0].Wind.Speed.Metric.Value,
+            humidity: api2Data[0].RelativeHumidity,
+            chanceOfRain: api3Data.DailyForecasts[0].Day.PrecipitationProbability,
           };
     
-          console.log(result);
+          activeCards.push(result);
+          // console.log(activeCards);
+          // console.log(result);
         })
         .catch(error => {
           console.error(error);
         });
+}
 
-    }
+const showCards = () => {
+  cardsContainer.innerHTML = ``
+  for(let card of activeCards) {
+    cardsContainer.innerHTML += `
+      <div class="card">
+        <div class="card-header">
+          <h3 class="city">${card.cityName}</h3>
+          <h3 class="uf">${card.country}</h3>
+          <!-- Se não for preciso a tag "a" trocar por span -->
+          <a href="#" class="close"><img src="../public/assets/x.svg" alt="close card"></a>
+        </div>
+        <div class="card-content">
+          <span class="weather-icon">
+            <img src="../public/assets/icons/${card.weatherIcon}.svg" alt="cloud">
+          </span>
+          <div class="actual-condition">
+            <h2>${card.temperature}°C</h2>
+            <p class="status">${card.currentCondition}</p>
+          </div>
+        </div>
+        <div class="condition-details">
+          <div class="info-container">
+            <img class="icon" src="../public/assets/wind.svg"  alt="Wind">
+            <p class="value">${card.windSpeed} km/h</p>
+            <p class="info-name">Wind</p>
+          </div>
+          <div class="info-container">
+            <img class="icon" src="../public/assets/droplet.svg"  alt="Humidity">
+            <p class="value">${card.humidity}%</p>
+            <p class="info-name">Humidity</p>
+          </div>
+          <div class="info-container">
+            <img class="icon" src="../public/assets/umbrella.svg"  alt="Chance of rain">
+            <p class="value">${card.chanceOfRain}%</p>
+            <p class="info-name last-info-name">Chance of Rain</p>
+          </div>
+        </div>
+      </div>
+    `
   }
+}
 
-  // activeCards.push(cardData);
-  // console.log(activeCards);
-
-  // console.log(conditions)
-  // console.log('oi')
-
-// teste();
 
 searchInput.addEventListener('change', function() {getSearchResult(createCardData)})
 form.addEventListener('submit', (event) => event.preventDefault())
-  
-export { resultKeys }
-
-
-// const conditions = []
-
-// fetch(`https://dataservice.accuweather.com/currentconditions/v1/41699?apikey=${apiKey}&language=pt-br&details=true`)
-//   .then(response => response.json())
-//   .then(data => {
-//     conditions.push(data[0])
-//   })
-//   .catch(err => console.error(err))
-
-// console.log(conditions)
-// const cardData = {
-//   cityName: 'Monte Aprazível',
-//   country: 'BR',
-//   temperature: '26',
-//   weatherCondition: 'Clear',
-//   icon: '7',
-//   wind: '14',
-//   Humidity: '60',
-//   ChanceOfRain: '0'
-// }
-
-// activeCards.push(cardData);
-// console.log(activeCards);
